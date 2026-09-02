b810976b4ee7233b420826074d3310fb08696b03
txgmail <tolyxiao@gmail.com>
2026-09-02 15:22:47 +0800

    build: expose core APIs

---
+++ src/game_helpers/core/__init__.py
@@ -0,0 +1,13 @@
+from .models import Action, ActionType, GameState, Point, Rect, WindowInfo
+from .window import find_window, list_windows
+
+__all__ = [
+    "Action",
+    "ActionType",
+    "GameState",
+    "Point",
+    "Rect",
+    "WindowInfo",
+    "find_window",
+    "list_windows",
+]
