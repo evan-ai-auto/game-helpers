05a1af5e374c59a66e368e874be3a75841ce78bb
txgmail <tolyxiao@gmail.com>
2026-09-02 17:55:05 +0800

    Export GameView discovery API

--- src/game_helpers/core/__init__.py
+++ src/game_helpers/core/__init__.py
@@ -1,4 +1,5 @@
 from .children import list_child_windows
+from .game_view import GameView, discover_game_views
 from .models import Action, ActionType, GameState, Point, Rect, WindowInfo
 from .window import find_window, list_windows
 
@@ -6,9 +7,11 @@ __all__ = [
     "Action",
     "ActionType",
     "GameState",
+    "GameView",
     "Point",
     "Rect",
     "WindowInfo",
+    "discover_game_views",
     "find_window",
     "list_child_windows",
     "list_windows",
