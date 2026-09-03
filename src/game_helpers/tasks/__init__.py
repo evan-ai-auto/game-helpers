996369dc59b3ddab6f093e6b4caca125953e4572
txgmail <tolyxiao@gmail.com>
2026-09-04 01:03:30 +0800

    refactor: export soul task detection primitives

--- src/game_helpers/tasks/__init__.py
+++ src/game_helpers/tasks/__init__.py
@@ -1,53 +1,15 @@
-"""Task, asset, route planning, diagnosis, progress, and render binding primitives."""
+"""Task, asset, route planning, diagnosis, progress, render binding, and task-state primitives."""
 
-from .diagnosis import (
-    CharacterSelection,
-    DiagnosisStatus,
-    TaskDiagnosis,
-    TaskDiagnosisEngine,
-    TaskExecutionPlan,
-    TaskObservation,
-    TaskRouteDecision,
-)
-from .models import (
-    AccountCandidate,
-    ItemAsset,
-    NpcAsset,
-    RoutePlan,
-    SceneAsset,
-    StepType,
-    TaskCategory,
-    TaskRecipe,
-    TaskStep,
-    TravelEdge,
-)
+from .diagnosis import CharacterSelection, DiagnosisStatus, TaskDiagnosis, TaskDiagnosisEngine, TaskExecutionPlan, TaskObservation, TaskRouteDecision
+from .models import AccountCandidate, ItemAsset, NpcAsset, RoutePlan, SceneAsset, StepType, TaskCategory, TaskRecipe, TaskStep, TravelEdge
 from .progress import LiveTaskEvidence, ResumeDecision, TaskProgress, TaskProgressStore, reconcile_progress
 from .render_binding import BindingStatus, RenderBinding, verify_render_binding
+from .soul_task import SoulTaskDetectionReason, SoulTaskObservation, SoulTaskStatus, SoulTaskUiProfile, detect_soul_task_claimed_icon
 
 __all__ = [
-    "AccountCandidate",
-    "BindingStatus",
-    "CharacterSelection",
-    "DiagnosisStatus",
-    "ItemAsset",
-    "LiveTaskEvidence",
-    "NpcAsset",
-    "RenderBinding",
-    "ResumeDecision",
-    "RoutePlan",
-    "SceneAsset",
-    "StepType",
-    "TaskCategory",
-    "TaskDiagnosis",
-    "TaskDiagnosisEngine",
-    "TaskExecutionPlan",
-    "TaskObservation",
-    "TaskProgress",
-    "TaskProgressStore",
-    "TaskRecipe",
-    "TaskRouteDecision",
-    "TaskStep",
-    "TravelEdge",
-    "reconcile_progress",
-    "verify_render_binding",
+    "AccountCandidate", "BindingStatus", "CharacterSelection", "DiagnosisStatus", "ItemAsset", "LiveTaskEvidence",
+    "NpcAsset", "RenderBinding", "ResumeDecision", "RoutePlan", "SceneAsset", "SoulTaskDetectionReason",
+    "SoulTaskObservation", "SoulTaskStatus", "SoulTaskUiProfile", "StepType", "TaskCategory", "TaskDiagnosis",
+    "TaskDiagnosisEngine", "TaskExecutionPlan", "TaskObservation", "TaskProgress", "TaskProgressStore", "TaskRecipe",
+    "TaskRouteDecision", "TaskStep", "TravelEdge", "detect_soul_task_claimed_icon", "reconcile_progress", "verify_render_binding",
 ]
