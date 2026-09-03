a49e2d0c6d60912e39769f9414e5e1abfa2edb8f
txgmail <tolyxiao@gmail.com>
2026-09-03 13:42:30 +0800

    feat: expose task and asset models

---
+++ src/game_helpers/tasks/__init__.py
@@ -0,0 +1,27 @@
+"""Task, asset, and route planning models."""
+
+from .models import (
+    AccountCandidate,
+    ItemAsset,
+    NpcAsset,
+    RoutePlan,
+    SceneAsset,
+    StepType,
+    TaskCategory,
+    TaskRecipe,
+    TaskStep,
+    TravelEdge,
+)
+
+__all__ = [
+    "AccountCandidate",
+    "ItemAsset",
+    "NpcAsset",
+    "RoutePlan",
+    "SceneAsset",
+    "StepType",
+    "TaskCategory",
+    "TaskRecipe",
+    "TaskStep",
+    "TravelEdge",
+]
