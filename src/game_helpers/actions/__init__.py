6b30e345c8467a5867f03964230ace5e65d7f825
txgmail <tolyxiao@gmail.com>
2026-09-04 23:44:44 +0800

    feat: export background visual click helper

--- src/game_helpers/actions/__init__.py
+++ src/game_helpers/actions/__init__.py
@@ -1,4 +1,10 @@
 from .background_input import BackgroundInput
+from .background_visual_click import BackgroundVisualClickResult, click_and_verify_visual_state
 from .executor import ActionExecutor
 
-__all__ = ["ActionExecutor", "BackgroundInput"]
+__all__ = [
+    "ActionExecutor",
+    "BackgroundInput",
+    "BackgroundVisualClickResult",
+    "click_and_verify_visual_state",
+]
