d1374791e50c03e821581ac40d156a46dd8f5014
txgmail <tolyxiao@gmail.com>
2026-09-04 14:50:17 +0800

    fix: align task exports with soul panel detector

--- src/game_helpers/tasks/__init__.py
+++ src/game_helpers/tasks/__init__.py
@@ -4,12 +4,13 @@ from .diagnosis import CharacterSelection, DiagnosisStatus, TaskDiagnosis, TaskD
 from .models import AccountCandidate, ItemAsset, NpcAsset, RoutePlan, SceneAsset, StepType, TaskCategory, TaskRecipe, TaskStep, TravelEdge
 from .progress import LiveTaskEvidence, ResumeDecision, TaskProgress, TaskProgressStore, reconcile_progress
 from .render_binding import BindingStatus, RenderBinding, verify_render_binding
-from .soul_task import SoulTaskDetectionReason, SoulTaskObservation, SoulTaskStatus, SoulTaskUiProfile, detect_soul_task_claimed_icon
+from .soul_task import SoulTaskDetectionReason, SoulTaskPanelObservation, SoulTaskStatus, SoulTaskUiProfile, detect_soul_task_claimed_icon, detect_soul_task_panel_collapsed
 
 __all__ = [
     "AccountCandidate", "BindingStatus", "CharacterSelection", "DiagnosisStatus", "ItemAsset", "LiveTaskEvidence",
     "NpcAsset", "RenderBinding", "ResumeDecision", "RoutePlan", "SceneAsset", "SoulTaskDetectionReason",
-    "SoulTaskObservation", "SoulTaskStatus", "SoulTaskUiProfile", "StepType", "TaskCategory", "TaskDiagnosis",
+    "SoulTaskPanelObservation", "SoulTaskStatus", "SoulTaskUiProfile", "StepType", "TaskCategory", "TaskDiagnosis",
     "TaskDiagnosisEngine", "TaskExecutionPlan", "TaskObservation", "TaskProgress", "TaskProgressStore", "TaskRecipe",
-    "TaskRouteDecision", "TaskStep", "TravelEdge", "detect_soul_task_claimed_icon", "reconcile_progress", "verify_render_binding",
+    "TaskRouteDecision", "TaskStep", "TravelEdge", "detect_soul_task_claimed_icon", "detect_soul_task_panel_collapsed",
+    "reconcile_progress", "verify_render_binding",
 ]
