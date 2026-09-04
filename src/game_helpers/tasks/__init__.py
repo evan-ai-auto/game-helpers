2510e0467d10cf9981273b7c1541a5c492c8d61b
txgmail <tolyxiao@gmail.com>
2026-09-04 15:00:41 +0800

    fix: export complete soul task detector API

--- src/game_helpers/tasks/__init__.py
+++ src/game_helpers/tasks/__init__.py
@@ -4,13 +4,13 @@ from .diagnosis import CharacterSelection, DiagnosisStatus, TaskDiagnosis, TaskD
 from .models import AccountCandidate, ItemAsset, NpcAsset, RoutePlan, SceneAsset, StepType, TaskCategory, TaskRecipe, TaskStep, TravelEdge
 from .progress import LiveTaskEvidence, ResumeDecision, TaskProgress, TaskProgressStore, reconcile_progress
 from .render_binding import BindingStatus, RenderBinding, verify_render_binding
-from .soul_task import SoulTaskDetectionReason, SoulTaskPanelObservation, SoulTaskStatus, SoulTaskUiProfile, detect_soul_task_claimed_icon, detect_soul_task_panel_collapsed
+from .soul_task import SoulTaskDetectionReason, SoulTaskObservation, SoulTaskPanelObservation, SoulTaskStatus, SoulTaskUiProfile, detect_soul_task_claimed_icon, detect_soul_task_panel_collapsed, inspect_task_panel_image, claim_verification_result
 
 __all__ = [
     "AccountCandidate", "BindingStatus", "CharacterSelection", "DiagnosisStatus", "ItemAsset", "LiveTaskEvidence",
     "NpcAsset", "RenderBinding", "ResumeDecision", "RoutePlan", "SceneAsset", "SoulTaskDetectionReason",
-    "SoulTaskPanelObservation", "SoulTaskStatus", "SoulTaskUiProfile", "StepType", "TaskCategory", "TaskDiagnosis",
-    "TaskDiagnosisEngine", "TaskExecutionPlan", "TaskObservation", "TaskProgress", "TaskProgressStore", "TaskRecipe",
-    "TaskRouteDecision", "TaskStep", "TravelEdge", "detect_soul_task_claimed_icon", "detect_soul_task_panel_collapsed",
-    "reconcile_progress", "verify_render_binding",
+    "SoulTaskObservation", "SoulTaskPanelObservation", "SoulTaskStatus", "SoulTaskUiProfile", "StepType", "TaskCategory",
+    "TaskDiagnosis", "TaskDiagnosisEngine", "TaskExecutionPlan", "TaskObservation", "TaskProgress", "TaskProgressStore",
+    "TaskRecipe", "TaskRouteDecision", "TaskStep", "TravelEdge", "claim_verification_result", "detect_soul_task_claimed_icon",
+    "detect_soul_task_panel_collapsed", "inspect_task_panel_image", "reconcile_progress", "verify_render_binding",
 ]
