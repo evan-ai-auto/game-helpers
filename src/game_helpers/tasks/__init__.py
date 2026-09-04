af5d389b8c5a15f1ecb7a99400a99c2f97defc3e
txgmail <tolyxiao@gmail.com>
2026-09-04 20:33:52 +0800

    feat: export item panel detector

--- src/game_helpers/tasks/__init__.py
+++ src/game_helpers/tasks/__init__.py
@@ -1,16 +1,19 @@
 """Task, asset, route planning, diagnosis, progress, render binding, and task-state primitives."""
 
 from .diagnosis import CharacterSelection, DiagnosisStatus, TaskDiagnosis, TaskDiagnosisEngine, TaskExecutionPlan, TaskObservation, TaskRouteDecision
+from .item_panel import DEFAULT_ITEM_PANEL_UI, ItemPanelObservation, ItemPanelStatus, ItemPanelUiProfile, compare_item_panel_state
 from .models import AccountCandidate, ItemAsset, NpcAsset, RoutePlan, SceneAsset, StepType, TaskCategory, TaskRecipe, TaskStep, TravelEdge
 from .progress import LiveTaskEvidence, ResumeDecision, TaskProgress, TaskProgressStore, reconcile_progress
 from .render_binding import BindingStatus, RenderBinding, verify_render_binding
 from .soul_task import SoulTaskDetectionReason, SoulTaskObservation, SoulTaskPanelObservation, SoulTaskStatus, SoulTaskUiProfile, detect_soul_task_claimed_icon, detect_soul_task_panel_collapsed, inspect_task_panel_image, claim_verification_result
 
 __all__ = [
-    "AccountCandidate", "BindingStatus", "CharacterSelection", "DiagnosisStatus", "ItemAsset", "LiveTaskEvidence",
-    "NpcAsset", "RenderBinding", "ResumeDecision", "RoutePlan", "SceneAsset", "SoulTaskDetectionReason",
-    "SoulTaskObservation", "SoulTaskPanelObservation", "SoulTaskStatus", "SoulTaskUiProfile", "StepType", "TaskCategory",
-    "TaskDiagnosis", "TaskDiagnosisEngine", "TaskExecutionPlan", "TaskObservation", "TaskProgress", "TaskProgressStore",
-    "TaskRecipe", "TaskRouteDecision", "TaskStep", "TravelEdge", "claim_verification_result", "detect_soul_task_claimed_icon",
+    "AccountCandidate", "BindingStatus", "CharacterSelection", "DiagnosisStatus", "ItemAsset", "ItemPanelObservation",
+    "ItemPanelStatus", "ItemPanelUiProfile", "LiveTaskEvidence", "NpcAsset", "RenderBinding", "ResumeDecision",
+    "RoutePlan", "SceneAsset", "SoulTaskDetectionReason", "SoulTaskObservation", "SoulTaskPanelObservation",
+    "SoulTaskStatus", "SoulTaskUiProfile", "StepType", "TaskCategory", "TaskDiagnosis", "TaskDiagnosisEngine",
+    "TaskExecutionPlan", "TaskObservation", "TaskProgress", "TaskProgressStore", "TaskRecipe", "TaskRouteDecision",
+    "TaskStep", "TravelEdge", "claim_verification_result", "compare_item_panel_state", "detect_soul_task_claimed_icon",
     "detect_soul_task_panel_collapsed", "inspect_task_panel_image", "reconcile_progress", "verify_render_binding",
+    "DEFAULT_ITEM_PANEL_UI",
 ]
