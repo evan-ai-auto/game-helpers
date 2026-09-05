4eb80538e9a00390258209dc455171dc677dbadd
txgmail <tolyxiao@gmail.com>
2026-09-05 13:09:13 +0800

    feat: export visual asset position type

--- src/game_helpers/tasks/__init__.py
+++ src/game_helpers/tasks/__init__.py
@@ -5,14 +5,15 @@ from .models import AccountCandidate, ItemAsset, NpcAsset, RoutePlan, SceneAsset
 from .progress import LiveTaskEvidence, ResumeDecision, TaskProgress, TaskProgressStore, reconcile_progress
 from .render_binding import BindingStatus, RenderBinding, verify_render_binding
 from .soul_task import SoulTaskDetectionReason, SoulTaskObservation, SoulTaskPanelObservation, SoulTaskStatus, SoulTaskUiProfile, detect_soul_task_claimed_icon, detect_soul_task_panel_collapsed, inspect_task_panel_image, claim_verification_result
-from .visual_state import VisualAnchor, VisualStateObservation, VisualStateProfile, VisualStateStatus, detect_visual_state, load_visual_state
+from .visual_state import VisualAnchor, VisualPositionType, VisualStateObservation, VisualStateProfile, VisualStateStatus, detect_visual_state, load_visual_state
 
 __all__ = [
     "AccountCandidate", "BindingStatus", "CharacterSelection", "DiagnosisStatus", "ItemAsset", "LiveTaskEvidence", "NpcAsset",
     "RenderBinding", "ResumeDecision", "RoutePlan", "SceneAsset", "SoulTaskDetectionReason", "SoulTaskObservation",
     "SoulTaskPanelObservation", "SoulTaskStatus", "SoulTaskUiProfile", "StepType", "TaskCategory", "TaskDiagnosis",
     "TaskDiagnosisEngine", "TaskExecutionPlan", "TaskObservation", "TaskProgress", "TaskProgressStore", "TaskRecipe",
-    "TaskRouteDecision", "TaskStep", "TravelEdge", "VisualAnchor", "VisualStateObservation", "VisualStateProfile",
-    "VisualStateStatus", "claim_verification_result", "detect_soul_task_claimed_icon", "detect_soul_task_panel_collapsed",
-    "detect_visual_state", "inspect_task_panel_image", "load_visual_state", "reconcile_progress", "verify_render_binding",
+    "TaskRouteDecision", "TaskStep", "TravelEdge", "VisualAnchor", "VisualPositionType", "VisualStateObservation",
+    "VisualStateProfile", "VisualStateStatus", "claim_verification_result", "detect_soul_task_claimed_icon",
+    "detect_soul_task_panel_collapsed", "detect_visual_state", "inspect_task_panel_image", "load_visual_state", "reconcile_progress",
+    "verify_render_binding",
 ]
