5b10fbcb3a0e8bf4d33aede78e54d7ea8e9a3f7f
txgmail <tolyxiao@gmail.com>
2026-09-05 17:33:53 +0800

    refactor: export shared verification session

--- src/game_helpers/tasks/__init__.py
+++ src/game_helpers/tasks/__init__.py
@@ -6,14 +6,15 @@ from .progress import LiveTaskEvidence, ResumeDecision, TaskProgress, TaskProgre
 from .render_binding import BindingStatus, RenderBinding, verify_render_binding
 from .soul_task import SoulTaskDetectionReason, SoulTaskObservation, SoulTaskPanelObservation, SoulTaskStatus, SoulTaskUiProfile, detect_soul_task_claimed_icon, detect_soul_task_panel_collapsed, inspect_task_panel_image, claim_verification_result
 from .visual_state import VisualAnchor, VisualPositionType, VisualStateObservation, VisualStateProfile, VisualStateStatus, detect_visual_state, load_visual_state
+from .verification_session import VerificationSession
 
 __all__ = [
     "AccountCandidate", "BindingStatus", "CharacterSelection", "DiagnosisStatus", "ItemAsset", "LiveTaskEvidence", "NpcAsset",
     "RenderBinding", "ResumeDecision", "RoutePlan", "SceneAsset", "SoulTaskDetectionReason", "SoulTaskObservation",
     "SoulTaskPanelObservation", "SoulTaskStatus", "SoulTaskUiProfile", "StepType", "TaskCategory", "TaskDiagnosis",
     "TaskDiagnosisEngine", "TaskExecutionPlan", "TaskObservation", "TaskProgress", "TaskProgressStore", "TaskRecipe",
-    "TaskRouteDecision", "TaskStep", "TravelEdge", "VisualAnchor", "VisualPositionType", "VisualStateObservation",
-    "VisualStateProfile", "VisualStateStatus", "claim_verification_result", "detect_soul_task_claimed_icon",
+    "TaskRouteDecision", "TaskStep", "TravelEdge", "VerificationSession", "VisualAnchor", "VisualPositionType",
+    "VisualStateObservation", "VisualStateProfile", "VisualStateStatus", "claim_verification_result", "detect_soul_task_claimed_icon",
     "detect_soul_task_panel_collapsed", "detect_visual_state", "inspect_task_panel_image", "load_visual_state", "reconcile_progress",
     "verify_render_binding",
 ]
