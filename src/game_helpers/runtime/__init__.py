c9898a847cb626c047f0d7f80de7e12b678b1aeb
txgmail <tolyxiao@gmail.com>
2026-09-06 23:40:54 +0800

    feat: export agent runtime

--- src/game_helpers/runtime/__init__.py
+++ src/game_helpers/runtime/__init__.py
@@ -1,6 +1,15 @@
 """Runtime orchestration for GUI Agent sessions."""
 
+from .agent_runtime import AgentRuntime, AgentStep, DefaultObservationBuilder, DefaultVerifier, MetadataGameAdapter
 from .session import BackgroundGameSession
 from .view_manager import GameViewManager
 
-__all__ = ["BackgroundGameSession", "GameViewManager"]
+__all__ = [
+    "AgentRuntime",
+    "AgentStep",
+    "BackgroundGameSession",
+    "DefaultObservationBuilder",
+    "DefaultVerifier",
+    "GameViewManager",
+    "MetadataGameAdapter",
+]
