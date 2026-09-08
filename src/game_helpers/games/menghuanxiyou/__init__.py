1d3ab162cbd215d1ab5b5766e8cfab891322b9e7
txgmail <tolyxiao@gmail.com>
2026-09-09 01:44:40 +0800

    feat: export soul task loop

--- src/game_helpers/games/menghuanxiyou/__init__.py
+++ src/game_helpers/games/menghuanxiyou/__init__.py
@@ -2,6 +2,29 @@
 
 from .agent import DreamAgent
 from .perception import DreamObservationBuilder
+from .soul_task import (
+    MILESTONES,
+    MilestoneState,
+    SoulRoundStatus,
+    SoulTaskLoop,
+    SoulTaskRound,
+    TaskExecutionResult,
+    TaskExecutionStatus,
+    TaskVerification,
+)
 from .state import DreamGameAdapter, DreamGameState
 
-__all__ = ["DreamAgent", "DreamGameAdapter", "DreamGameState", "DreamObservationBuilder"]
+__all__ = [
+    "DreamAgent",
+    "DreamGameAdapter",
+    "DreamGameState",
+    "DreamObservationBuilder",
+    "MILESTONES",
+    "MilestoneState",
+    "SoulRoundStatus",
+    "SoulTaskLoop",
+    "SoulTaskRound",
+    "TaskExecutionResult",
+    "TaskExecutionStatus",
+    "TaskVerification",
+]
