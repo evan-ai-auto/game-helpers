53f8967b0cc0c9ac41b658aa8ac23a978d353571
txgmail <tolyxiao@gmail.com>
2026-09-09 01:41:05 +0800

    feat: export multi-character task control

--- src/game_helpers/runtime/__init__.py
+++ src/game_helpers/runtime/__init__.py
@@ -2,14 +2,38 @@
 
 from .agent_runtime import AgentRuntime, AgentStep, DefaultObservationBuilder, DefaultVerifier, MetadataGameAdapter
 from .session import BackgroundGameSession
+from .task_control import (
+    AssignmentConflict,
+    AssignmentDecision,
+    CharacterRegistry,
+    CharacterRuntimeState,
+    CharacterScheduler,
+    CharacterSession,
+    MultiCharacterRuntime,
+    RunMode,
+    RuntimeStatus,
+    TaskAssignment,
+    TaskRegistry,
+)
 from .view_manager import GameViewManager
 
 __all__ = [
     "AgentRuntime",
     "AgentStep",
+    "AssignmentConflict",
+    "AssignmentDecision",
     "BackgroundGameSession",
+    "CharacterRegistry",
+    "CharacterRuntimeState",
+    "CharacterScheduler",
+    "CharacterSession",
     "DefaultObservationBuilder",
     "DefaultVerifier",
     "GameViewManager",
     "MetadataGameAdapter",
+    "MultiCharacterRuntime",
+    "RunMode",
+    "RuntimeStatus",
+    "TaskAssignment",
+    "TaskRegistry",
 ]
